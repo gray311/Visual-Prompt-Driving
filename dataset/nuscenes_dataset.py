9ba9719e4bdecf89dd95b4284f92e0a07831066b
Yingzi Ma <64787301+gray311@users.noreply.github.com>
2024-10-15 00:02:23 +0800

    Update code

--- dataset/nuscenes_dataset.py
+++ dataset/nuscenes_dataset.py
@@ -1,13 +1,28 @@
 import nuscenes
 import os
+import random
+from PIL import Image
 from nuscenes.nuscenes import NuScenes
 from nuscenes.can_bus.can_bus_api import NuScenesCanBus
 from nuscenes.utils.data_classes import Box, Quaternion
 from nuscenes.eval.common.utils import quaternion_yaw, Quaternion
 from nuscenes.utils.geometry_utils import box_in_image
+
+from nuscenes.map_expansion.map_api import NuScenesMap, NuScenesMapExplorer
+from nuscenes.map_expansion import arcline_path_utils
+from nuscenes.map_expansion.bitmap import BitMap
+
+import descartes
+from pyquaternion import Quaternion
+from nuscenes.utils.geometry_utils import view_points
+from shapely.geometry import Polygon, MultiPolygon, LineString, Point, box
+
 from .utils import relative_heading_angle, get_rotation_matrix
 from .traj_api import NuScenesTraj
 import numpy as np
+
+import matplotlib.pyplot as plt
+
 DEFAULT_SENSOR_CHANNEL = 'CAM_FRONT'
 
 
@@ -65,6 +80,13 @@ class NuscenesLoader:
             past_steps=4,
             fut_steps=4,
         )
+        self.nusc_map_dict = {
+            "boston-seaport": NuScenesMap(dataroot=dataroot, map_name='boston-seaport'),
+            "singapore-onenorth": NuScenesMap(dataroot=dataroot, map_name='singapore-onenorth'),
+            "singapore-hollandvillage": NuScenesMap(dataroot=dataroot, map_name='singapore-hollandvillage'),
+            "singapore-queenstown": NuScenesMap(dataroot=dataroot, map_name='singapore-queenstown'),
+        }
+
         self.frequency = frequency
 
     def get_scene_description(self, scene_id):
@@ -125,7 +147,155 @@ class NuscenesLoader:
         can_bus[-2] = patch_angle / 180 * np.pi
         can_bus[-1] = patch_angle
 
-        return np.array(can_bus)
+        return np.array(can_bus), rotation
+
+        
+    def _get_map_segmentation(self, sample_token, intersection_threshold=10000):
+        import time
+
+        t1 = time.time()
+        # hyperparameters
+        patch_radius = 50
+        near_plane = 1e-8
+        min_polygon_area: float = 100
+        render_outside_im = True
+
+        layer_names = ['lane']
+        camera_channel = DEFAULT_SENSOR_CHANNEL
+        sample_record = self.nusc.get('sample', sample_token)
+        scene_record = self.nusc.get('scene', sample_record['scene_token'])
+        log_record = self.nusc.get('log', scene_record['log_token'])
+        log_location = log_record['location']
+
+        # Grab the front camera image and intrinsics.
+        cam_token = sample_record['data'][camera_channel]
+        cam_record = self.nusc.get('sample_data', cam_token)
+        cam_path = self.nusc.get_sample_data_path(cam_token)
+        image = im = Image.open(cam_path)
+        im_size = im.size
+        cs_record = self.nusc.get('calibrated_sensor', cam_record['calibrated_sensor_token'])
+        cam_intrinsic = np.array(cs_record['camera_intrinsic'])
+
+        # Retrieve the current map.
+        ego_pose = self.nusc.get('ego_pose', cam_record['ego_pose_token'])
+        ego_translation = ego_pose['translation']
+
+        box_coords = (
+            ego_translation[0] - patch_radius,
+            ego_translation[1] - patch_radius,
+            ego_translation[0] + patch_radius,
+            ego_translation[1] + patch_radius,
+        )
+
+        scene = self.nusc.get('scene', sample_record['scene_token'])
+        log = self.nusc.get('log', scene['log_token'])
+        map_name = log['location']
+        map_api = self.nusc_map_dict[map_name]
+        records_in_patch = map_api.explorer.get_records_in_patch(box_coords, layer_names, 'intersect')
+    
+
+        polygons, center_points, labels, center_3dpoints = [], [], [], []
+        for layer_name in layer_names:
+            for token in records_in_patch[layer_name]:
+                record = map_api.get(layer_name, token)
+                if layer_name == 'drivable_area':
+                    polygon_tokens = record['polygon_tokens']
+                else:
+                    polygon_tokens = [record['polygon_token']]
+
+                for polygon_token in polygon_tokens:
+                    polygon = map_api.extract_polygon(polygon_token)
+
+                    # Convert polygon nodes to pointcloud with 0 height.
+                    points = np.array(polygon.exterior.xy)
+                    poly = Polygon(points.T)
+                    points = np.vstack((points, np.zeros((1, points.shape[1]))))
+
+                     
+                    # Transform into the ego vehicle frame for the timestamp of the image.
+                    points = points - np.array(ego_pose['translation']).reshape((-1, 1))
+                    points = np.dot(Quaternion(ego_pose['rotation']).rotation_matrix.T, points)
+
+                    # Transform into the camera.
+                    points = points - np.array(cs_record['translation']).reshape((-1, 1))
+                    points = np.dot(Quaternion(cs_record['rotation']).rotation_matrix.T, points)
+
+                    # Remove points that are partially behind the camera.
+                    depths = points[2, :]
+                    behind = depths < near_plane
+                    if np.all(behind):
+                        continue
+
+                    # if render_behind_cam:
+                        # Perform clipping on polygons that are partially behind the camera.
+                    points = NuScenesMapExplorer._clip_points_behind_camera(points, near_plane)
+                    # elif np.any(behind):
+                    #     # Otherwise ignore any polygon that is partially behind the camera.
+                    #     continue
+
+                    # Ignore polygons with less than 3 points after clipping.
+                    if len(points) == 0 or points.shape[1] < 3:
+                        continue
+
+                    # Take the actual picture (matrix multiplication with camera-matrix + renormalization).
+                    points = view_points(points, cam_intrinsic, normalize=True)
+
+                    # Skip polygons where all points are outside the image.
+                    # Leave a margin of 1 pixel for aesthetic reasons.
+                    inside = np.ones(points.shape[1], dtype=bool)
+                    inside = np.logical_and(inside, points[0, :] > 1)
+                    inside = np.logical_and(inside, points[0, :] < im.size[0] - 1)
+                    inside = np.logical_and(inside, points[1, :] > 1)
+                    inside = np.logical_and(inside, points[1, :] < im.size[1] - 1)
+                    if render_outside_im:
+                        if np.all(np.logical_not(inside)):
+                            continue
+                    else:
+                        if np.any(np.logical_not(inside)):
+                            continue
+
+                    points = points[:2, :]
+                    # points[0, :] = np.clip(points[0, :], 0, im.size[0] - 1)
+                    # points[1, :] = np.clip(points[1, :], 0, im.size[1] - 1)
+                    points_2d = [(p0, p1) for (p0, p1) in zip(points[0], points[1])]
+                    
+                    polygon_proj = Polygon(points_2d)
+                    
+                    polygon_im = Polygon([(0, 0), (0, im.size[1]), (im.size[0], im.size[1]), (im.size[0], 0)])
+
+                    # Filter small polygons
+                    if polygon_proj.area < min_polygon_area:
+                        continue
+
+                  
+                    intersection_polygon = polygon_proj.intersection(polygon_im)
+                    if intersection_polygon.area < intersection_threshold:
+                        continue
+
+                    point = np.array(polygon_proj.intersection(polygon_im).centroid.coords)
+                    polygons.append(intersection_polygon)
+                    center_points.append(point[0])
+                    center_3dpoints.append(poly)
+                    # labels.append(label)
+        labels = []
+        min_dist, ego_lane_idx = 10000, None
+        for i, center_3dpoly in enumerate(center_3dpoints):
+            point = Point(ego_translation[0], ego_translation[1])
+            distance = point.distance(center_3dpoly)
+            if distance <= min_dist:
+                min_dist = distance
+                ego_lane_idx = i 
+        
+        for i, center_3dpoint in enumerate(center_3dpoints):
+            if i == ego_lane_idx:
+                label = "ego"
+            else:
+                label = "object"
+            labels.append(label)
+
+        ego_lane = None
+        return polygons, center_points, labels, map_name, ego_lane 
+    
 
     def get_sample_description(self, sample_token):
         # each sample is a frame in the scene consisting of a set of annotations in different sensors
@@ -133,7 +303,11 @@ class NuscenesLoader:
         sample = self.nusc.get('sample', sample_token)
         sdc_fut_traj_all, sdc_fut_traj_valid_mask_all = self.traj_api.get_sdc_traj_label(sample_token)
 
-        can_bus = self._get_can_bus_info(sample)
+        scene = self.nusc.get('scene', sample['scene_token'])
+        log = self.nusc.get('log', scene['log_token'])
+        map_name = log['location']
+
+        can_bus, rotation = self._get_can_bus_info(sample)
         ego_pose_token = self.nusc.get('sample_data', sample['data'][DEFAULT_SENSOR_CHANNEL])['ego_pose_token']
         ego_pose = self.nusc.get('ego_pose', ego_pose_token)
 
@@ -147,18 +321,22 @@ class NuscenesLoader:
                 self.reference_pose,
                 self.reference_rotation_matrix
         )
-        # print(location,heading)
+
         sample_description = {
             "ego_vehicle_location": location,
             "ego_vehicle_heading": heading,
             "ego_vehicle_velocity": can_bus[-3],
             "ego_vehicle_accelerate": can_bus[7],
-            "ego_vehicle_yawangle": can_bus[-1],
+            "ego_vehicle_yawangle": can_bus[-2],
+            "ego_vehicle_rotation": rotation,
             "ego_future_trajectory": sdc_fut_traj_all,
             "ego_future_trajectory_mask": sdc_fut_traj_valid_mask_all,
             "timestamp": timestamp,
             "filepath": filepath,
-            "sample_annotations": object_descriptions
+            "sample_annotations": object_descriptions,
+            "city": map_name,
+            "sample_token": sample_token,
+
         }
         next_sample_token = sample['next']
         return sample_description, next_sample_token
@@ -222,7 +400,7 @@ class NuscenesLoader:
                 box.translate(-np.array(cs_record['translation']))
                 box.rotate(Quaternion(cs_record['rotation']).inverse)
 
-                if box_in_image(box, cam_intrinsic, imsize, vis_level=0):
+                if box_in_image(box, cam_intrinsic, imsize, vis_level=1):
                     ann['center_point'] = center_point
                     ann['bounding_box'] = bbox
                     ann['instance_token'] = ann_token
@@ -311,5 +489,7 @@ if __name__ == "__main__":
     ## Test the function
     loader = NuscenesLoader(version="v1.0-trainval", dataroot="/data/yingzi_ma/Visual-Prompt-Driving/workspace/nuscenes")
     metadata = loader.load(0)
-    # print(metadata)
+    sample_token = metadata[0]['sample_token']
+    polygons, points, labels, map_name, ego_lane = loader._get_map_segmentation(sample_token, intersection_threshold=10000)
+
     
